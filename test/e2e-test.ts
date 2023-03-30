46e582e0a1d5a6c7277e6e89d93143e891fa2c10
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-03-30 12:23:10 +1100

    feat: add test for query

--- test/e2e-test.ts
+++ test/e2e-test.ts
@@ -1,10 +1,10 @@
 import { ITestSuiteConfig, TestSuiteRunner } from '../lib/TestSuiteRunner';
 import { testCaseToMediaMappings, QueryResultBindings, QueryResultBoolean, QueryResultQuads, ErrorSkipped } from '..';
-// import { QueryEngine } from '@comunica/query-sparql';
-// import { Store as N3Store } from 'n3';
+import { QueryEngine } from '@comunica/query-sparql';
+import { Store as N3Store } from 'n3';
 import rdfParse from 'rdf-parse';
 import { JsonLdParser } from 'jsonld-streaming-parser';
-// import { ProxyHandlerStatic } from '@comunica/actor-http-proxy';
+import { ProxyHandlerStatic } from '@comunica/actor-http-proxy';
 import * as path from 'path';
 import * as fs from 'fs';
 
@@ -90,85 +90,100 @@ describe('e2e tests on the test suite runner', () => {
     }
   });
 
+  describe('query', () => {
+    for (const spec of [
+      "http://www.w3.org/TR/sparql11-query/",
+      "http://www.w3.org/TR/sparql11-update/",
+      "http://www.w3.org/TR/sparql11-results-csv-tsv/",
+      "http://www.w3.org/TR/sparql11-results-json/",
+      // Federated spec tries to do external queries
+      // "http://www.w3.org/TR/sparql11-federated-query/",
+      "http://www.w3.org/TR/sparql11-service-description/",
+      "http://www.w3.org/TR/sparql11-protocol/",
+      "http://www.w3.org/TR/sparql11-http-rdf-update/",
+    ]) {
+      
+    it(`should run correctly on [${spec}]`, async () => {
+    config.specification = spec
+    const result = await runner.runManifest('http://w3c.github.io/rdf-tests/sparql/sparql11/manifest-all.ttl', queryEngine(new QueryEngine()), config);
+
+    // Run assertions
+    expect(console.log).not.toHaveBeenCalled();
+    // Warnings occur when the runner cannot handle a test
+    expect(console.warn).not.toHaveBeenCalled();
+    // One error for each of the tsv cases
+    expect(console.error).toHaveBeenCalledTimes(6);
+
+    expect(result.length).toBeGreaterThan(0)
+
+    let skipped = 0;
+    result.forEach(r => {
+      if (r.skipped)
+      skipped++;
+    });
+
+    // Unsupported test can be skipped
+    if (!spec.includes('tsv') && !spec.includes('rdf-update') && !spec.includes('service-description') && !spec.includes('protocol'))
+      expect(skipped).toEqual(0);
+
+    expect(result.every(r => r.ok || r.skipped)).toEqual(true);
   
+  }, 130_000);
+    }
 
-  // TODO: Get this working
-  // describe('query', () => {
-  //   it('should run correctly on [http://w3c.github.io/rdf-tests/sparql/sparql11/manifest-all.ttl]', async () => {
-  //     const result = await runner.runManifest('http://w3c.github.io/rdf-tests/sparql/sparql11/manifest-all.ttl', queryEngine(new QueryEngine()), config);
-
-  //     // Run assertions
-  //     expect(console.log).not.toHaveBeenCalled();
-  //     // Warnings occur when the runner cannot handle a test
-  //     expect(console.warn).not.toHaveBeenCalled();
-  //     // expect(console.warn).toHaveBeenCalledWith(new Error("Could not parse the query result with content type text/tab-separated-values at http://w3c.github.io/rdf-tests/sparql/sparql11/csv-tsv-res/csvtsv02.tsv"))
-
-  //     // One error for each of the tsv cases
-  //     expect(console.error).toHaveBeenCalledTimes(6);
-
-  //     expect(result.length).toBeGreaterThan(0)
-
-
-  //     // runner.resultsToText(process.stdout, result, false);
-  //     console.log(result)
-
-  //     expect(result.every(r => r.ok)).toEqual(true);
-    
-  //   }, 130_000);
-  // });
+  });
 });
 
+function queryEngine(engine) {
+  return {
+    parse: function (query, options) {
+      return engine.actorInitQuery.mediatorQueryParse.mediate({ query: query, baseIRI: options.baseIRI });
+    },
+    query: function(data, queryString, options) {
+      return this.queryLdf([{ type: 'rdfjsSource', value: source(data) }], null, queryString, options);
+    },
+    queryLdf: async function(sources, proxyUrl, queryString, options) {
+      const result = await engine.query(queryString, {
+        baseIRI: options.baseIRI,
+        sources,
+        httpProxyHandler: proxyUrl ? new ProxyHandlerStatic(proxyUrl) : null,
+        httpRetryOnServerError: true,
+        httpRetryCount: 3,
+        httpRetryDelay: 10,
+      });
+      if (result.resultType === 'boolean') {
+        return new QueryResultBoolean(await result.execute());
+      } else if (result.resultType === 'quads') {
+        return new QueryResultQuads(await require('arrayify-stream').default(await result.execute()));
+      } else if (result.resultType === 'bindings') {
+        return new QueryResultBindings(
+            (await result.metadata()).variables.map(variable => `?${variable.value}`),
+            (await require('arrayify-stream').default(await result.execute()))
+              .map((binding) => Object.fromEntries([ ...binding ]
+                  .map(([ key, value ]) => [ `?${key.value}`, value ]))), false
+        );
+      } else {
+        throw new Error('Invalid query result type: ' + result.resultType);
+      }
+    },
+    update: async function(data, queryString, options) {
+      const store = await source(data);
+      const result = await engine.query(queryString, {
+        baseIRI: options.baseIRI,
+        source: { type: 'rdfjsSource', value: store },
+        destination: store,
+      });
+      await result.execute();
+      return [...store];
+    },
+  };
+};
 
-// function queryEngine(engine) {
-//   return {
-//     parse: function (query, options) {
-//       return engine.actorInitQuery.mediatorQueryParse.mediate({ query: query, baseIRI: options.baseIRI });
-//     },
-//     query: function(data, queryString, options) {
-//       return this.queryLdf([{ type: 'rdfjsSource', value: source(data) }], null, queryString, options);
-//     },
-//     queryLdf: async function(sources, proxyUrl, queryString, options) {
-//       const result = await engine.query(queryString, {
-//         baseIRI: options.baseIRI,
-//         sources,
-//         httpProxyHandler: proxyUrl ? new ProxyHandlerStatic(proxyUrl) : null,
-//         httpRetryOnServerError: true,
-//         httpRetryCount: 3,
-//         httpRetryDelay: 10,
-//       });
-//       if (result.resultType === 'boolean') {
-//         return new QueryResultBoolean(await result.execute());
-//       } else if (result.resultType === 'quads') {
-//         return new QueryResultQuads(await require('arrayify-stream').default(await result.execute()));
-//       } else if (result.resultType === 'bindings') {
-//         return new QueryResultBindings(
-//             (await result.metadata()).variables.map(variable => `?${variable.value}`),
-//             (await require('arrayify-stream').default(await result.execute()))
-//               .map((binding) => Object.fromEntries([ ...binding ]
-//                   .map(([ key, value ]) => [ `?${key.value}`, value ]))), true
-//         );
-//       } else {
-//         throw new Error('Invalid query result type: ' + result.resultType);
-//       }
-//     },
-//     update: async function(data, queryString, options) {
-//       const store = await source(data);
-//       const result = await engine.query(queryString, {
-//         baseIRI: options.baseIRI,
-//         source: { type: 'rdfjsSource', value: store },
-//         destination: store,
-//       });
-//       await result.execute();
-//       return [...store];
-//     },
-//   };
-// };
-
-// function source(data) {
-//   const store = new N3Store();
-//   store.addQuads(data);
-//   return store;
-// }
+function source(data) {
+  const store = new N3Store();
+  store.addQuads(data);
+  return store;
+}
 
 const normalParser = {
   parse(data, baseIRI, _, test) {
